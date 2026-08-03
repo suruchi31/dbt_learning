fff7f022377408da39910548005fc817f8be24be
Suruchi Bedi <suruchi.bedi@coforge.com>
2026-08-03 09:30:28 +0000

    staging level completed except few models

---
+++ models/Intermediate/int_suppliers.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='incremental', unique_key='supplier_id') }}
+{# if this (materialized='incremental') is not writen at the top then it will create a create statement, 
+but in this case it will create insert statement #}
+
+with suppliers as (
+    select 
+    supplier.supplier_id,
+    supplier.supplier_name,
+    supplier.supplier_address,
+    supplier.phone_number,
+    supplier.account_balance,
+    supplier.updated_time
+
+from {{ ref('stg_suppliers') }} supplier
+{%if (is_incremental()) %}
+where updated_time > (select max(updated_time) from {{this}} )
+{% endif %}
+)
+
+select * from suppliers
